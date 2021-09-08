fc89dbbfefe7e71e4b66cb9158fd71ab2af90907
Alexis190392 <alexis19031992@gmail.com>
2021-09-07 21:46:01 -0300

    comienzo

---
+++ src/main/java/com/trees/treeSave/TreeSaveApplication.java
@@ -0,0 +1,13 @@
+package com.trees.treeSave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeSaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trees/treeSave/TreeSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trees.treeSave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
