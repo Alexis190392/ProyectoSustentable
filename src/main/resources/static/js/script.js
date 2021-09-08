ace840d48f2d4c3c22cf03ecb62bb1a3c00db117
Alexis190392 <alexis19031992@gmail.com>
2021-09-08 00:50:54 -0300

    agrego css en produccion

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,20 @@
+$('#handleCounter').handleCounter()
+
+$('#handleCounter').handleCounter({
+    
+      minimum: 1,
+    
+      maximize:null,
+    
+    })
+    
+    $('#handleCounter').handleCounter({
+        
+          onChange:function(){},
+        
+          onMinimum:function(){},
+        
+          onMaximize:function(){}
+        
+        })
+        
