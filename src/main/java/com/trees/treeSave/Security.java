60fff11c791ac004f9ae46f0c2ef444f3834c287
IvanBamBam96 <yvan.ortega.96@gmail.com>
2021-09-23 21:52:37 -0300

    guardo para sacar datos del login

---
+++ src/main/java/com/trees/treeSave/Security.java
@@ -0,0 +1,14 @@
+/*
+hacemos autenticacion : usuario q se pueda logear
+hacemos autorizaciones : lo que tiene permitido cada usuario.
+ */
+
+package com.trees.treeSave;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Security {
+
+}
---
+++ src/main/java/com/trees/treeSave/services/UserService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.trees.treeSave.services;
+
+/**
+ * 
+ * @author Ivan Doom Days
+ */
+public class UserService {
+
+}
