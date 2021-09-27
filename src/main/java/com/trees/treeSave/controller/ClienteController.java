6be19080e2fc441121d2dac0433005f14d2d64c3
Alexis190392 <alexis19031992@gmail.com>
2021-09-27 14:38:51 -0300

    panel SuperSu - producto 90% + controller

--- src/main/java/com/trees/treeSave/controller/ClienteController.java
+++ src/main/java/com/trees/treeSave/controller/ClienteController.java
@@ -16,10 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/**
- *
- * @author Fede
- */
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController {
